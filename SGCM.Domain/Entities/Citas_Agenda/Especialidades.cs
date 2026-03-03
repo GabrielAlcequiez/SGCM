eb422d0ecb6502a57da1452a89509b969a4a3701
Gabriel Alcequiez <20251062@itla.edu.do>
2026-03-02 22:50:32 -0400

    fix: implementar ExcepcionValidacion en vez de Excepction en entidad Especialidades

--- SGCM.Domain/Entities/Citas_Agenda/Especialidades.cs
+++ SGCM.Domain/Entities/Citas_Agenda/Especialidades.cs
@@ -1,4 +1,5 @@
 ﻿using SGCM.Domain.Base;
+using SGCM.Domain.Exceptions;
 
 namespace SGCM.Domain.Entities.Medicos
 {
@@ -19,13 +20,13 @@ namespace SGCM.Domain.Entities.Medicos
         protected override void ValidarEntradaDatos()
         {
             if (string.IsNullOrEmpty(Nombre))
-                throw new Exception("El nombre de la especialidad no puede estar vacío.");
+                throw new ExcepcionValidacion("El nombre de la especialidad no puede estar vacío.");
             if (Nombre.Length > 50
-                ) throw new Exception("El nombre de la especialidad no debe sobrepasar los 50 caracteres");
+                ) throw new ExcepcionValidacion("El nombre de la especialidad no debe sobrepasar los 50 caracteres");
             if (string.IsNullOrEmpty(Descripcion))
-                throw new Exception("La descripción no puede estar vacia");
+                throw new ExcepcionValidacion("La descripción no puede estar vacia");
             if (Descripcion.Length > 200)
-                throw new Exception("La descripción no debe sobrepasar los 200 caracteres");
+                throw new ExcepcionValidacion("La descripción no debe sobrepasar los 200 caracteres");
 
 
 
