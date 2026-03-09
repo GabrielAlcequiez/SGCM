6f85aa0d6b08b5a6dac86c8aa79a8f28d155c40d
Gabriel Alcequiez <20251062@itla.edu.do>
2026-03-09 09:31:30 -0400

    feat: implementar AuditoriaLogger

---
+++ SGCM.Application/Logger/AuditoriaLogger.cs
@@ -0,0 +1,22 @@
+﻿using SGCM.Domain.Entities.Seguridad_Usuarios;
+using SGCM.Domain.Repository;
+
+namespace SGCM.Application.Logger
+{
+    public class AuditoriaLogger : IAuditoriaLogger
+    {
+        private readonly IAuditoriaLogsRepository _logsRepository;
+
+        public AuditoriaLogger(IAuditoriaLogsRepository repository)
+        {
+            _logsRepository = repository;
+        }
+        public async Task RegistrarAsync(int usuarioId, string accion, string entidadAfectada, string detalles)
+        {
+            var audi = new AuditoriaLogs(usuarioId, accion, entidadAfectada, detalles);
+
+            await _logsRepository.AgregarAsync(audi);
+
+        }
+    }
+}
---
+++ SGCM.Application/Logger/IAuditoriaLogger.cs
@@ -0,0 +1,7 @@
+﻿namespace SGCM.Application.Logger
+{
+    public interface IAuditoriaLogger
+    {
+        Task RegistrarAsync(int usuarioId, string accion, string entidadAfectada, string detalles);
+    }
+}
